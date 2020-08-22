731a7e34d0b790c4ccf0ca0fc486789f65c40ceb
Mostafa <m.tavassoli.it@gmail.com>
2020-08-22 23:52:34 +0200

    feat: add validation on Login Form and config the Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import UserRole from "./components/UserRole";
+import LoginForm from "./components/LoginForm";
 import DataTable from "./components/DataTable";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { AuthContext } from "./auth/AuthContext";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { authErr: "", authUser: "" };
   }
 
+  // componentDidMount() {
+  //   //check if the user is authenticated
+  //   API.isAuthenticated()
+  //     .then((user) => {
+  //       this.setState({ authUser: user });
+  //     })
+  //     .catch((err) => {
+  //       this.setState({ authErr: err.errorObj });
+  //       this.props.history.push("/login");
+  //     });
+  // }
+
+  // handleErrors(err) {
+  //   if (err) {
+  //     if (err.status && err.status === 401) {
+  //       this.setState({ authErr: err.errorObj });
+  //       this.props.history.push("/login");
+  //     }
+  //   }
+  // }
+
   render() {
+    const value = {
+      authUser: this.state.authUser,
+      authErr: this.state.authErr,
+      loginUser: this.login,
+      logoutUser: this.logout,
+    };
+
     return (
-      <div className="App">
-        <UserRole />;
-        <DataTable
-          classes={["table", "table-bordered"]}
-          header={["name", "age"]}
-          data={[
-            { name: "test", age: 20 },
-            { name: "test", age: 20 },
-          ]}
-        />
-      </div>
+      <AuthContext.Provider value={value}>
+        <div className="App">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Redirect from="/" to="/login" />
+          </Switch>
+          {/* <LoginForm />; */}
+          <DataTable
+            classes={["table", "table-bordered"]}
+            header={["name", "age"]}
+            data={[
+              { name: "test", age: 20 },
+              { name: "test", age: 20 },
+            ]}
+          />
+        </div>
+      </AuthContext.Provider>
     );
   }
 }
---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,131 @@
+import React from "react";
+import {
+  Form,
+  FormGroup,
+  Col,
+  Button,
+  FormControl,
+  Container,
+  Row,
+} from "react-bootstrap";
+import { Alert } from "reactstrap";
+import { AuthContext } from "../auth/AuthContext";
+
+class UserRole extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      pass: "",
+      stNO: "",
+      role: "teacher",
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // console.log();
+    alert(JSON.stringify(this.state));
+    this.setState({
+      email: "",
+      pass: "",
+      stNO: "",
+    });
+  }
+  render() {
+    return (
+      <AuthContext.Consumer>
+        {(context) => (
+          <Container>
+            <Row>
+              <Col md={{ span: 4, offset: 4 }}>
+                <h3> Select you Role : </h3>
+
+                <Form method="POST" onSubmit={this.handleSubmit}>
+                  <FormGroup>
+                    <select
+                      className="form-control"
+                      role={this.state.value}
+                      onChange={this.handleInputChange}
+                      name="role"
+                    >
+                      <option value="teacher">Teacher</option>
+                      <option value="student">Student</option>
+                    </select>
+                  </FormGroup>
+
+                  {this.state.role == "teacher" && (
+                    <>
+                      <FormGroup>
+                        <FormControl
+                          type="email"
+                          id="email"
+                          name="email"
+                          placeholder="Email"
+                          value={this.state.email}
+                          onChange={this.handleInputChange}
+                          required
+                          autoFocus
+                        />
+                      </FormGroup>
+                      <FormGroup>
+                        <FormControl
+                          type="password"
+                          id="pass"
+                          name="pass"
+                          placeholder="Password"
+                          value={this.state.pass}
+                          onChange={this.handleInputChange}
+                          required
+                        />
+                      </FormGroup>
+                    </>
+                  )}
+
+                  {this.state.role === "student" && (
+                    <>
+                      <FormGroup>
+                        <FormControl
+                          type="text"
+                          id="stNO"
+                          name="stNO"
+                          placeholder="Student Number"
+                          value={this.state.stNO}
+                          onChange={this.handleInputChange}
+                          required
+                        />
+                      </FormGroup>
+                    </>
+                  )}
+
+                  <FormGroup>
+                    <Button variant="primary" type="submit" color="primary">
+                      Login
+                    </Button>
+                  </FormGroup>
+                </Form>
+                {context.authErr && (
+                  <Alert variant="danger">{context.authErr.msg}</Alert>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        )}
+      </AuthContext.Consumer>
+    );
+  }
+}
+
+export default UserRole;
